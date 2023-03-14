732d04b45d5d387ceb10a86c46d98175b81d15e3
Bartexs <barttosz.witkowski@gmail.com>
2023-03-14 19:48:45 +0100

    Bids view func

--- src/app/models/bid-item-model.ts
+++ src/app/models/bid-item-model.ts
@@ -1,3 +1,15 @@
 export class Bid { 
-    
+    public readonly id: number;
+    public readonly email: string;
+    public readonly price: number;
+    public readonly bidTime: string;
+    public readonly auctionId: number;
+
+    public constructor(id: number, email: string, price: number, bidTime: string, auctionId: number) {
+        this.id = id;
+        this.email = email;
+        this.price = price;
+        this.bidTime = bidTime;
+        this.auctionId = auctionId;
+    }
 }
--- src/app/views/bids-view/bids-view.component.ts
+++ src/app/views/bids-view/bids-view.component.ts
@@ -1,7 +1,9 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Auction } from 'src/app/models/auction-item-model';
 import { Bid } from 'src/app/models/bid-item-model';
 import { DialogData } from '../pending-auctions-view-component/pending-auction-item/pending-auction-item.service';
+import { BidsViewService } from './bids-view.service';
 
 @Component({
   selector: 'app-bids-view',
@@ -9,13 +11,23 @@ import { DialogData } from '../pending-auctions-view-component/pending-auction-i
   styleUrls: ['./bids-view.component.scss']
 })
 export class BidsViewComponent implements OnInit {
-  private bidsList: Bid[] = [];
+  public auctionId!: number;
+
+  public bidsList: Bid[] = [];
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: DialogData
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private readonly bidsViewService: BidsViewService,
     ) { }
 
   ngOnInit(): void {
+    this.auctionId = this.data.auctionId;
+    this.subscribeToGetBidsList(this.auctionId);
   }
 
+  private subscribeToGetBidsList(auctionId: number): void {
+    this.bidsViewService.getBidsList().subscribe((data) => {
+      this.bidsList = this.bidsViewService.sortListByHighestBid(this.bidsViewService.filterBidsList(auctionId, data));
+    })
+  };
 }
